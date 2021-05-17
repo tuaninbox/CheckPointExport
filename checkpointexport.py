19ba7a49f80c92d93d63294b56433f7849de4ff8
Tuan Hoang <tuaninbox@gmail.com>
2021-05-17 11:53:05 +0800

    Add Network Group

--- checkpointexport.py
+++ checkpointexport.py
@@ -17,20 +17,19 @@ def checkcredential():
         print("The following environment variables need to be set")
         print("mgmthost: {}".format(mgmt_host))
         print("mgmtport: {}".format(mgmt_port))
-        print("username: {}".format(username))
-        print("password: {}".format("*"))
+        print("user: {}".format(username))
+        print("pass: {}".format(len(password)))
         return 0
     else:
         return 1
 
-def getrulelist(input,typeofinput):
+def getnumberlist(input,typeofinput):
     result = []
     if typeofinput=="f":
         with open(input,"r") as f:
             line = f.readlines()
     else:
         line = input.split()
-    #print(type(line),line)
     for l in line:
         l=l.rstrip("\n")
         if "-" in l:
@@ -46,7 +45,7 @@ def getrulelist(input,typeofinput):
             result.append(l)
     return result
 
-def getapplicationsitelist(input,typeofinput):
+def getnamelist(input,typeofinput):
     result=[]
     if typeofinput=="f":
         with open(input,"r") as f:
@@ -69,13 +68,11 @@ def api_call(ip_addr, port, command, json_payload, sid):
     else:
         request_headers = {'Content-Type' : 'application/json', 'X-chkp-sid' : sid}
     r = requests.post(url,data=json.dumps(json_payload), headers=request_headers,verify=False)
-    #print(r)
     return r.json()
 
 def login(host, port, user,password):
     payload = {'user':user, 'password' : password}
     response = api_call(host, port, 'login',payload, '')
-    #print(response)
     return response["sid"]
 
 def logout(user,password,sid):
@@ -85,14 +82,9 @@ def logout(user,password,sid):
     #print("logout result: " + json.dumps(logout_result))				
 
 def get_key(data,key1,key2):
-    #print data
-    #print key
-    #print data.get("original-source").get("ipv4-address-first")
     data1=data.get(key1)
-    #print "\n", data1
     try:
         if key2 == "":
-    #	print "Key2: ",key2
             return str(data1)
         else:
             if key2 not in data1:
@@ -109,13 +101,10 @@ def getaccesslayers(server,port,sid):
     print(json.dumps(result))
     formatted_table = json2html.convert(json=result)
     print(formatted_table)
-        #print result
-        #print ("{0:>8s}{1:>14s}{2:>14s}{3:>14s}{4:>14s}{5:>20s}{6:>14s}  {7:<70s}".format(str(rulenumber), str(result["original-source"]["ipv4-address-first"]), str(result["original-destination"]["ipv4-address"]), str(result["original-service"]["name"]), str(result["translated-source"]["ipv4-address"]), str(result["translated-destination"]["ipv4-address"]), str(result["translated-service"]["name"]), str(result["comments"])))
 
 def getnatrule(server,port,rulelist,sid):
     command = 'show-nat-rule'
     policy = 'DC_Policy'
-    #print("{0:>8s}{1:>30s}{2:>14s}{3:>10s}{4:>14s}{5:>14s}{6:>20s}{7:>20s}   {8:<70s}".format("Rule","Source","Destination","Port","Protocol","NAT_Src","NAT_Dest","NAT_Services","Comment"))
     header=["Rule","Source","Destination","Port","Protocol","NAT_Src","NAT_Dest","NAT_Services","Comment"]
     listofrule=[]
     listofrule.append(header)
@@ -133,22 +122,17 @@ def getnatrule(server,port,rulelist,sid):
         rule.append(get_key(result,'translated-service','name')) #6
         rule.append(get_key(result,'comments','')) #7
         listofrule.append(rule)
-    #print ("{0:>8s}{1:>30s}{2:>14s}{3:>10s}{4:>14s}{5:>14s}{6:>20s}{7:>20s}   {8:<70s}".format(str(rulenumber), rule[0], rule[1], rule[2],rule[3], rule[4], rule[5], rule[6], rule[7]))
     return listofrule
 
 def getaccessrule(server,port,rulelist,sid):
     command = 'show-access-rule'
     layer = 'DC_Policy Security'
-    #layer = 'School_8030_InlinePolicy_Outbound'
     header=["Rule","Name","Source","Destination","Services","VPN","Content","Action","Time","Track","Install On","Comment"]
     listofrule=[]
     listofrule.append(header)
     for rulenumber in rulelist:
         host_data = {'rule-number':rulenumber, 'layer':layer}
         result = api_call(mgmt_host, mgmt_port,command, host_data ,sid)
-        #print(rulenumber)
-        #print(result)
-        #sys.exit(2)
         rule=[]
         rule.append(rulenumber)
         rule.append(get_key(result,'name',"")) #0
@@ -194,8 +178,6 @@ def getaccessrule(server,port,rulelist,sid):
             rule.append("NOT ("+listofdestination[:-3]+")")
         listofservice=""
         for r in result['service']:
-            #for s in result['service']:
-            #    service=s['name']+(" - port: ") +s['port']
             if r['name'] == "Any" or r['type']=="service-group":
                 service=r['name'] + " + "
             elif r['type']=="service-tcp":
@@ -221,15 +203,10 @@ def getaccessrule(server,port,rulelist,sid):
             inlinerule=getaccessruleinline(mgmt_host,mgmt_port,rulenumber,totalinlinerule,inlinelayer,sid,0)
             for r in inlinerule:
                 listofrule.append(r)
-    #    return result['source']
     return listofrule
-    #formatted_table = json2html.convert(json=result)
-    #print(formatted_table)
-    #return formatted_table
 
 def getaccessruleinline(server,port,parentrule,total,layer,sid,addheader):
     command = 'show-access-rule'
-    #layer = 'School_8030_InlinePolicy_Outbound'
     header=["Rule","Name","Source","Destination","Services","VPN","Content","Action","Time","Track","Install On","Comment"]
     listofrule=[]
     if addheader == 1:
@@ -237,9 +214,6 @@ def getaccessruleinline(server,port,parentrule,total,layer,sid,addheader):
     for rulenumber in range(1,total+1):
         host_data = {'rule-number':rulenumber, 'layer':layer}
         result = api_call(mgmt_host, mgmt_port,command, host_data ,sid)
-        #print(rulenumber)
-        # print(result)
-        # sys.exit(2)
         rule=[]
         rule.append(str(parentrule)+"."+str(rulenumber))
         rule.append(get_key(result,'name',"")) #0
@@ -247,18 +221,10 @@ def getaccessruleinline(server,port,parentrule,total,layer,sid,addheader):
         for r in result['source']:
             for k,v in r.items():
                 if k == 'type' and v == 'host':
-                    # print(k,": ",v,end='')
-                    # print("name:",r['name'],end='')
-                    # print("IP: ",r['ipv4-address'])
                     source = r['name'] + " - IP: " + r['ipv4-address'] + " + "
                 if k == 'type' and v == 'network':
-                    # print(k,": ",v,end='')
-                    # print("name:",r['name'],end='')
-                    # print("Net: ",r['subnet4'])
                     source = r['name'] + " - Net: " + r['subnet4'] + "/" + str(r['mask-length4']) + " + "
                 elif k == 'type' and v == 'group':
-                    # print(k,": ",v, end='')
-                    # print("name:",r['name'])
                     source = r['name'] + " + "
                 elif k == 'type' and v == 'CpmiAnyObject':
                     source = r['name'] + " + "
@@ -285,8 +251,6 @@ def getaccessruleinline(server,port,parentrule,total,layer,sid,addheader):
             rule.append("NOT ("+listofdestination[:-3]+")")
         listofservice=""
         for r in result['service']:
-            #for s in result['service']:
-            #    service=s['name']+(" - port: ") +s['port']
             if r['name'] == "Any" or r['type']=="service-group" or r['type']=="application-site":
                 service=r['name'] + " + "
             elif r['type']=="service-tcp":
@@ -309,11 +273,7 @@ def getaccessruleinline(server,port,parentrule,total,layer,sid,addheader):
         rule.append(result['install-on'][0]['name'])
         rule.append(result['comments'])            
         listofrule.append(rule)
-    #    return result['source']
     return listofrule
-    #formatted_table = json2html.convert(json=result)
-    #print(formatted_table)
-    #return formatted_table
 
 def getaccessrulebase(server,port,layer,sid):
     command = 'show-access-rulebase'
@@ -330,25 +290,42 @@ def getaccessrulebase(server,port,layer,sid):
 
 def getapplicationsite(server,port,names,sid):
     command = 'show-application-site'
-    #layer = 'DC_Policy Security'
     header=["Name","Primary-Category","URL-List"]
     listofrule=[]
     listofrule.append(header)
     for name in names:
         host_data = {'name':name}
         result = api_call(mgmt_host, mgmt_port,command, host_data ,sid)
-        # print(name)
-        # print(result)
-        # sys.exit(2)
         rule=[]
         rule.append(name)
         rule.append(result['primary-category'])
         for r in result['url-list']:
             rule.append(r)
-            #rule.append(",")
         listofrule.append(rule)    
     return listofrule
-    
+
+def getnetworkgroup(server,port,groups,sid):
+    command = 'show-group'
+    header=["GroupName","Name","Type","Address"]
+    output=[]
+    output.append(header)
+    for groupname in groups:
+        host_data = {'name':groupname}
+        result = api_call(mgmt_host, mgmt_port,command, host_data ,sid)
+        for r in result['members']:
+            listofmembers=[]
+            listofmembers.append(groupname)
+            listofmembers.append(r['name'])
+            listofmembers.append(r['type'])
+            if r['type'] == 'host':
+                listofmembers.append(r['ipv4-address'])
+            if r['type'] == 'network':
+                listofmembers.append(r['subnet4']+"/"+str(r['mask-length4']))
+            if r['type'] == 'address-range':
+                listofmembers.append(r['ipv4-address-first']+"-"+str(r['ipv4-address-last']))
+            output.append(listofmembers)
+    return output
+
 def printresult(data,printto,format,filename="output.txt"):
     try:
         output=""
@@ -373,6 +350,7 @@ def printresult(data,printto,format,filename="output.txt"):
         print(e)
 
 def main():
+#Menu
     parser=argparse.ArgumentParser(description='Check Point Policy Management')
     parser.add_argument('-w','--writefile',type=str,metavar='',help='File to write output to')
     group1=parser.add_mutually_exclusive_group(required=True)
@@ -382,17 +360,18 @@ def main():
     group.add_argument('-n', '--nat',action='store_true', help='nat policy')
     group.add_argument('-a', '--access',action='store_true', help='security access')
     group.add_argument('-as', '--applicationsite',action='store_true', help='applicaiton site')
+    group.add_argument('-g', '--group',action='store_true', help='network group')
     args=parser.parse_args()
 
 #GET Rule List or Application Site Name List
-    if args.applicationsite and args.file:
-        rulelist=getapplicationsitelist(args.file,"f")
-    elif args.applicationsite and args.rule:
-        rulelist=getapplicationsitelist(args.rule,"r")
+    if args.applicationsite or args.group and args.file:
+        rulelist=getnamelist(args.file,"f")
+    elif args.applicationsite or args.group and args.rule:
+        rulelist=getnamelist(args.rule,"r")
     elif args.file:
-        rulelist=getrulelist(args.file,"f")
+        rulelist=getnumberlist(args.file,"f")
     elif args.rule:
-        rulelist=getrulelist(args.rule,"r")
+        rulelist=getnumberlist(args.rule,"r")
 
 #CHECK if credential set  
     if checkcredential():
@@ -401,10 +380,8 @@ def main():
         print("Please set host and credential!")
         sys.exit(1)
 
-
 #NAT RULE
     if args.nat:
-        #result=[]
         result=getnatrule(mgmt_host,mgmt_port,rulelist,sid)
         if not args.writefile:
             printresult(result,"stdout","csv")
@@ -412,8 +389,7 @@ def main():
             printresult(result,"file","csv",args.writefile)
 
 #ACCESS RULE
-    if args.access:    
-        #result=[]
+    if args.access:
         result = getaccessrule(mgmt_host,mgmt_port,rulelist,sid)
         if not args.writefile:
             printresult(result,"stdout","csv")
@@ -422,12 +398,20 @@ def main():
 
 #Application Site
     if args.applicationsite:
-        applicationsites=["Manually_Allowed_Sites,Manually_Blocked_Sites"]
-        result=getapplicationsite(mgmt_host,mgmt_port,applicationsites,sid)
+        result=getapplicationsite(mgmt_host,mgmt_port,rulelist,sid)
         if not args.writefile:
             printresult(result,"stdout","csv")
         else:
             printresult(result,"file","csv",args.writefile)
+    
+#Network Group
+    if args.group:
+        result=getnetworkgroup(mgmt_host,mgmt_port,rulelist,sid)
+        if not args.writefile:
+            printresult(result,"stdout","csv")
+        else:
+            printresult(result,"file","csv",args.writefile)
+        
 
 #Access Rule Base
     # inlinelayer="InlinePolicy_Outbound"
